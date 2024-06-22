71aea57b166720c59343ed27a39be981c5d6d923
Diksha Lanjewar <dikshalanjewar0109@gmail.com>
2024-06-22 20:19:14 +0530

    adding files

---
+++ HTML/JS/lecture18.js
@@ -0,0 +1,31 @@
+// import { Component } from "react";
+// class TodoScreen extends Component{
+//     render(){
+//         return(
+//             <h1 className="ui-heading center">To Do List</h1>
+//         )
+//     }
+// }
+// export default TodoScreen;
+
+
+
+
+
+
+// @import url(https://cdn.jsdelivr.net/npm/semantic-ui@2.5.0/dist/semantic.min.css);
+
+// body {
+
+// margin: 0;
+
+// font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue","sans-serif";
+
+// -webkit-font-smoothing: antialiased;
+
+// -moz-osx-font-smoothing: grayscale;
+// }
+// code {
+
+// font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
+// }
