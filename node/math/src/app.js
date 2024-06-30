52e03d81a678ba839d4c7de86a8d010cb9a335d8
Diksha Lanjewar <dikshalanjewar0109@gmail.com>
2024-06-30 15:31:24 +0530

    adding files

--- node/math/src/app.js
+++ node/math/src/app.js
@@ -1,6 +1,15 @@
-const addNumbers = (...args)=>{
-    let sum =0;
-    args.forEach((arg)=> (sum += args));
-    return sum;
-};
-console.log(addNumbers(4,5,1,-2,10,5));
+// const addNumbers = (...args)=>{
+//     let sum =0;
+//     args.forEach((arg)=> (sum += args));
+//     return sum;
+// };
+// console.log(addNumbers(4,5,1,-2,10,5));
+const http = require("http");
+const server = http.createServer((req,res)=>{
+res.write("This is some response from your first node.js server");
+res.end();
+});
+
+server.listen(3003,()=>{
+    console.log("Server is running");
+});
