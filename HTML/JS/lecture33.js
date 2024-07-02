68d529767b087ee7825d4f48e8f85d33a7cbcaf9
Diksha Lanjewar <dikshalanjewar0109@gmail.com>
2024-07-02 19:16:56 +0530

    Adding files

---
+++ HTML/JS/lecture33.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+app.get("/",(req,res)=>{
+    res.send("This is some response from your first node.js server")
+});
+app.get("/add",(req,res)=>{
+    let{a: firstNumber,b: SecondNumber} = req.query;
+    let sum = parseInt(firstNumber)+parseInt(SecondNumber);
+    res.send({sum});
+});
+app.listen(8080,()=>{
+    console.log("Server is running");
+});
