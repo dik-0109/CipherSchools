d6d154a5bd2b2d0a9a92914b0c580fda9f07d1f1
Diksha Lanjewar <dikshalanjewar0109@gmail.com>
2024-07-02 19:14:30 +0530

    adding files

--- node/math/src/app.js
+++ node/math/src/app.js
@@ -4,12 +4,33 @@
 //     return sum;
 // };
 // console.log(addNumbers(4,5,1,-2,10,5));
-const http = require("http");
-const server = http.createServer((req,res)=>{
-res.write("This is some response from your first node.js server");
-res.end();
-});
+// const http = require("http");
+// const server = http.createServer((req,res)=>{
+// res.write("This is some response from your first node.js server");
+// res.end();
+// });
 
-server.listen(3003,()=>{
+// server.listen(3003,()=>{
+//     console.log("Server is running");
+// });
+// const express = require("express");
+// const app = express();
+// app.get("/add",(req,res)=>{
+//     res.send("This is some response from your first node.js server")
+// });
+// app.listen(8080,()=>{
+//     console.log("Server is running");
+// });
+const express = require("express");
+const app = express();
+app.get("/",(req,res)=>{
+    res.send("This is some response from your first node.js server")
+});
+app.get("/add",(req,res)=>{
+    let{a: firstNumber,b: SecondNumber} = req.query;
+    let sum = parseInt(firstNumber)+parseInt(SecondNumber);
+    res.send({sum});
+});
+app.listen(8080,()=>{
     console.log("Server is running");
 });
