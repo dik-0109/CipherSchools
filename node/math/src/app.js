47d943d87000b474a2e20c3bda28087bc056fcd1
Diksha Lanjewar <dikshalanjewar0109@gmail.com>
2024-07-06 17:34:21 +0530

    Adding files

--- node/math/src/app.js
+++ node/math/src/app.js
@@ -21,6 +21,7 @@
 // app.listen(8080,()=>{
 //     console.log("Server is running");
 // });
+require("./appMongoose");
 const express = require("express");
 const app = express();
 app.get("/",(req,res)=>{
@@ -31,6 +32,11 @@ app.get("/add",(req,res)=>{
     let sum = parseInt(firstNumber)+parseInt(SecondNumber);
     res.send({sum});
 });
+app.post("/add-task",(req,res)=>{
+    const task = new Task({title:"Test Task", description : "Test Task Desc"});
+    await task.save();
+    return res.status(201).send({message:saved})
+})
 app.listen(8080,()=>{
     console.log("Server is running");
 });
